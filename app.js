4995daf5431f728c0f04e2762c27547f51fbb659
Adelkilde <William.adelkilde@gmail.com>
2023-03-29 21:48:37 +0200

    BETA Version
    JS, HTML, CSS færdig(?)

--- app.js
+++ app.js
@@ -4,8 +4,18 @@ window.addEventListener("load", initApp);
 
 async function initApp() {
     const pokemonner = await getPokemonner();
+    pokemonner.sort(compareDexindex);
     showPokemonner(pokemonner);
+    document.addEventListener("keydown", function(event) {
+        if (event.key === "Escape") {
+          closeDialog();
+        }
+      });
     // pokemonner.forEach(addPokemon);
+}
+
+function compareDexindex(pokemonA, pokemonB) {
+    return pokemonA.dexindex - pokemonB.dexindex;
 }
 
 async function getPokemonner(){
@@ -24,7 +34,9 @@ function showPokemon(pokemon) {
     let myHTML = /*html*/ 
     `<article id="pokemon-container">
         <img src=${pokemon.image}>
-        <b>${pokemon.name}</b> - <b>${pokemon.dexindex}</b><br>
+        <b>${pokemon.name}</b> 
+        <b>-</b> 
+        <b>#${pokemon.dexindex}</b><br>
     </article>
     `;
     
@@ -41,8 +53,8 @@ function showPokemon(pokemon) {
         <li><b>Type:</b> ${pokemon.type}</li>
         <li><b>Subtype: </b>${pokemon.subtype}</li>
         <li><b>Gender:</b> ${pokemon.gender}</li>
-        <li><b>Weight:</b> ${pokemon.weight}</li>
-        <li><b>Height:</b> ${pokemon.height}</li>
+        <li><b>Weight:</b> ${pokemon.weight} g</li>
+        <li><b>Height:</b> ${pokemon.height} m</li>
         <li><b>Generation:</b> ${pokemon.generation}</li>
         <li><b>Can evolve:</b> ${pokemon.canEvolve}</li>
         <li><b>Ability: </b>${pokemon.ability}</li>
@@ -60,6 +72,7 @@ function showPokemon(pokemon) {
     document.querySelector("#dialog-pokemon-info").insertAdjacentHTML("beforeend", pokemonDexInfo);
 
     document.querySelector("#dialog-pokemon-info").showModal(pokemon);
+    document.querySelector("#dialog-pokemon-info").scrollTop = 0;
     document.querySelector("#detailed-info-btn").addEventListener("click", closeDialog);
     }
 }
@@ -68,4 +81,3 @@ function closeDialog() {
     document.querySelector("#dialog-pokemon-info").close();
     document.querySelector("#pokemon-info-list").remove();
   }
-  
